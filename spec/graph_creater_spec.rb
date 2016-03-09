dde8fdee4d3ec3e2cc969f94a8c804d559917681
shadow3x3x3 <shadow3x3x3@gmail.com>
2016-03-09 14:02:03 +0800

    check edges

---
+++ spec/graph_creater_spec.rb
@@ -0,0 +1,19 @@
+require "spec_helper"
+require_relative "../graph_creater.rb"
+
+describe GraphCreater do
+
+  describe "edge check" do
+    it "edges out of range" do
+      gc = GraphCreater.new(node: 2, edge: 3, dim: 4)
+      expect(gc.edge_out_of_range?).to eq(true)
+    end
+
+    it "edges not out of range" do
+      gc = GraphCreater.new(node: 3, edge: 3, dim: 4)
+      expect(gc.edge_out_of_range?).to eq(false)
+    end
+  end
+
+
+end
