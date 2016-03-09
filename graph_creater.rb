b01ab6d79f99bb64da670f6fa4a99ca6b9be3cba
sheng wei qiu <shadow3x3x3@gmail.com>
2016-03-09 16:57:05 +0800

    create easy graph but not perfect

--- graph_creater.rb
+++ graph_creater.rb
@@ -1,12 +1,43 @@
 class GraphCreater
   def initialize(node: nil, edge: nil, dim: 4)
-    @node = node
-    @edge = edge
-    @dim  = dim
+    @node   = node
+    @edge   = edge
+    @dim    = dim
+    @result = self.create
+  end
+
+  def create
+    result_array  = []
+    created_edges = 0
+    current_node  = 0
+
+    # find all nodes
+    until created_edges == @edge
+      current = 0 if current_node == @node
+      current_node += 1
+      connect_node  = 0
+      # find all nodes that may connect
+      until connect_node == @node || created_edges == @edge
+        connect_node += 1
+        next if current_node == connect_node # no loop self
+        # connect or not
+        if rand(4) == 0
+          created_edges += 1
+          result_array << [current_node, connect_node]
+        end
+      end
+    end # find all nodes end
+
+    result_array
+  end
+
+  def show_edges
+    p @result
   end
 
   def edge_out_of_range?
-    @node * (@node-1) / 2 < @edge ? true : false
+    max_edges = @node * (@node-1) / 2
+    max_edges < @edge ? true : false
   end
 
   def create_dim
