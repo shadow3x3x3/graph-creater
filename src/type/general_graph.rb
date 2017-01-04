6b83c5e5fffa483c67bdfe45c093a2b753c5004b
sheng wei qiu <shadow3x3x3@gmail.com>
2017-01-04 15:03:28 +0800

    Using Duck Typing

--- src/type/general_graph.rb
+++ src/type/general_graph.rb
@@ -1,8 +1,4 @@
 class GeneralGraph
-  def generate(nodes_num, edges_num)
-    raise NotImplementedError, "Implement this method in a child class"
-  end
-
   protected
 
   def two_permutation_array(array)
